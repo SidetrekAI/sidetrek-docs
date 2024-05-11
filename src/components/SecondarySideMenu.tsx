7714b278d99a6c460c964a5b08e142206d27408f
Seungchan Lee <seunggs@gmail.com>
2024-05-11 00:12:16 -0700

    fix container width and add right side menu

---
+++ src/components/SecondarySideMenu.tsx
@@ -0,0 +1,9 @@
+import { HEADER_HEIGHT } from '@/lib/constants'
+
+export default function SecondarySideMenu() {
+  return (
+    <nav className="shrink-0 hidden xl:block w-[200px] h-fit pl-4 pr-4 py-6 text-sm text-slate-600 border-b border-slate-200 overflow-auto">
+      PLACEHOLDER
+    </nav>
+  )
+}
